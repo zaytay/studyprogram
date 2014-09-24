a71f16c6aacdf4f402636c8ca2eebff64cfce4a8
zaytay <zaytay@163.com>
2014-09-24 16:23:25 +0800

    add list

---
+++ src/main/java/ListStudy.java
@@ -0,0 +1,13 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: zhengxuetong
+ * Date: 14-9-24
+ * Time: 下午3:57
+ * Desc:
+ */
+public class ListStudy {
+    public static void main(String args[]){
+        System.out.println("ceshi");
+    }
+
+}
