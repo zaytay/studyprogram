9cd47a45487e4ce35aa19d705d2a547d89e66199
zaytay <zaytay@163.com>
2014-09-24 18:20:08 +0800

    list

--- src/main/java/ListStudy.java
+++ src/main/java/ListStudy.java
@@ -10,4 +10,28 @@ public class ListStudy {
         System.out.println("ceshi");
     }
 
+    //链表节点结构
+    private static class Node{
+        int val;
+        Node next;
+        public Node(int val){
+            this.val=val;
+        }
+    }
+
+    public static void printList(Node head){
+        while (head != null){
+            System.out.print(head.val+"  ");
+            head=head.next;
+        }
+        System.out.println();
+    }
+
+    //遍历
+    public static Node reverseList(Node head){
+          return head;
+    }
+
+
+
 }
